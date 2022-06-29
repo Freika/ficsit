6d84823a1fb6a6ec07fda30bd7accd139cc40f4f
Eugene Burmakin <iamfrey@gmail.com>
2022-06-30 00:10:10 +0300

    Consider double input into machine during calculation

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gemspec
 gem "rake", "~> 13.0"
 
 gem 'byebug'
+gem 'terminal-table'
 
 gem "rspec", "~> 3.0"
 
--- lib/satisfactory_calculator.rb
+++ lib/satisfactory_calculator.rb
@@ -2,28 +2,35 @@
 
 require_relative "satisfactory_calculator/version"
 require 'byebug'
+require 'terminal-table'
 
 module SatisfactoryCalculator
   class Error < StandardError; end
 
   class Calc
     attr_reader :inputs
+    attr_reader :tables
 
     def initialize(amount)
       @amount = amount
       @inputs = []
+      @tables = []
     end
 
     def call
       screws = recipes.find { |a| a[:name] == 'Screws' }
 
       machines = machines_amount(@amount, screws[:out])
-      @inputs << calculate_input(screws[:in], machines)
+      @inputs << calculate_input(screws, machines)
+
+      inputs = @inputs.reverse.reject(&:empty?)
+
+      @tables.each { |t| puts t }
 
       {
         name: screws[:name],
         machines: machines,
-        inputs: @inputs.flatten.reverse
+        inputs: inputs
       }
     end
 
@@ -31,8 +38,9 @@ module SatisfactoryCalculator
       [
         { name: 'Screws', out: 40, in: [{ name: 'Iron rod', pieces: 10 }] },
         { name: 'Iron rod', out: 15, in: [{ name: 'Iron ingot', pieces: 15 }] },
-        { name: 'Iron ingot', out: 30, in: [{ name: 'Iron', pieces: 30 }] },
-        { name: 'Iron', out: 30, in: [] }
+        { name: 'Iron ingot', out: 30, in: [{ name: 'Iron', pieces: 30 }, { name: 'Copper', pieces: 30 }] },
+        { name: 'Iron', out: 30, in: [] },
+        { name: 'Copper', out: 30, in: [] }
       ]
     end
 
@@ -40,19 +48,21 @@ module SatisfactoryCalculator
       (amount / out).to_f.round(2)
     end
 
-    def calculate_input(incoming_resource, machines)
-      incoming_resource.flat_map do |input_resource|
+    def calculate_input(recipe, machines)
+      recipe[:in].flat_map do |input_resource|
         pieces_total = machines * input_resource[:pieces]
 
-        recipe = recipes.find { |a| a[:name] == input_resource[:name] }
-
-        @inputs << calculate_input(recipe[:in], machines_amount(pieces_total, recipe[:out]))
-
-        {
+        recipe_found = recipes.find { |a| a[:name] == input_resource[:name] }
+        machines_number = machines_amount(pieces_total, recipe_found[:out])
+        result = {
           name: input_resource[:name],
           pieces_total: pieces_total.round(2),
-          machines: machines_amount(pieces_total, recipe[:out]),
+          machines: machines_number
         }
+
+        @inputs << calculate_input(recipe_found, machines_number)
+
+        result
       end
     end
   end
