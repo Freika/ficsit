34bf9b95885ed9dbd2181b68fd737a5d357fb43f
Eugene Burmakin <iamfrey@gmail.com>
2022-06-30 00:31:05 +0300

    Implement drawing tables

--- lib/satisfactory_calculator.rb
+++ lib/satisfactory_calculator.rb
@@ -25,7 +25,7 @@ module SatisfactoryCalculator
 
       inputs = @inputs.reverse.reject(&:empty?)
 
-      @tables.each { |t| puts t }
+      puts draw_tables
 
       {
         name: screws[:name],
@@ -45,24 +45,59 @@ module SatisfactoryCalculator
     end
 
     def machines_amount(amount, out)
-      (amount / out).to_f.round(2)
+      (amount / out).to_f
     end
 
     def calculate_input(recipe, machines)
-      recipe[:in].flat_map do |input_resource|
-        pieces_total = machines * input_resource[:pieces]
+      table_rows = []
+
+      calculated = recipe[:in].flat_map do |input_resource|
+        pieces_total = (machines * input_resource[:pieces])
 
         recipe_found = recipes.find { |a| a[:name] == input_resource[:name] }
         machines_number = machines_amount(pieces_total, recipe_found[:out])
         result = {
           name: input_resource[:name],
-          pieces_total: pieces_total.round(2),
+          pieces_total: pieces_total,
           machines: machines_number
         }
 
         @inputs << calculate_input(recipe_found, machines_number)
+        table_rows << table_row(input_resource[:name], pieces_total)
 
         result
+      end
+
+      table_data = {
+        name: recipe[:name],
+        rows: table_rows,
+        machines: machines.truncate(2),
+        output: (recipe[:out] * machines).truncate(3)
+      }
+
+      @tables << table_data
+
+      calculated
+    end
+
+    def table_row(name, pieces_total)
+      [name, pieces_total]
+    end
+
+    def draw_tables
+      @tables.reverse.each do |data|
+        next if data[:rows].empty?
+
+        rows = data[:rows]
+        rows << :separator
+        rows << ['Machines', data[:machines]]
+        rows << :separator
+        rows << ['Output', data[:output]]
+
+        puts Terminal::Table.new(
+          title: data[:name],
+          rows: rows
+        )
       end
     end
   end
